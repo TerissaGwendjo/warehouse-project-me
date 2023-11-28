aca3b449bafb06fab4a4b5467a30d2565eddc2d2
dci-student <Terissa>
2023-11-28 14:26:19 +0100

    Major changes made in Employee and User classes

--- main/src/personnel/Employee.java
+++ main/src/personnel/Employee.java
@@ -1,4 +1,39 @@
 package personnel;
 
-public class Employee {
+public class Employee extends User {
+    //since Employee is a User, we need more information to differentiate them from other users.
+    private String employeeId;
+    private String jobTitle;
+
+    //Constructor
+    public Employee(String name, String password, String employeeId, String jobTitle) {
+        super(name, password);
+        this.employeeId = employeeId;
+        this.jobTitle = jobTitle;
+    }
+
+    //Getters and setters
+    public String getEmployeeId() {
+        return employeeId;
+    }
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Employee {name=" + getUser_name() + ", password=" + getPassword() +
+                "employeeId='" + employeeId + '\'' +
+                ", jobTitle='" + jobTitle + '\'' +
+                '}';
+    }
 }
