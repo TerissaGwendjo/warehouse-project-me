a46cdba441d3277ecd93de28958e033b6bcf0b93
dci-student <Terissa>
2023-11-28 18:53:41 +0100

    Major changes made in Employee and User classes

--- main/src/stock/Warehouse.java
+++ main/src/stock/Warehouse.java
@@ -1,4 +1,32 @@
 package stock;
 
+import java.util.List;
+
 public class Warehouse {
+    private int id;
+    private List <Integer> stock;
+
+    public Warehouse(int id, List<Integer> stock) {
+        this.id = id;
+        this.stock = stock;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public List<Integer> getStock() {
+        return stock;
+    }
+    public void setStock(List<Integer> stock) {
+        this.stock = stock;
+    }
+    @Override
+    public String toString() {
+        return "Warehouse{" +
+                "id=" + id +
+                ", stock=" + stock +
+                '}';
+    }
 }
